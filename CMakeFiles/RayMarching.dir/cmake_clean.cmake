8b4c4e88560fe5980b7f15494a0ad3d2711b419e
aliagagf <aliagaguilherme@gmail.com>
2025-06-08 17:37:24 -0300

    first commit

---
+++ CMakeFiles/RayMarching.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayMarching.dir/RayMarching.cpp.obj"
+  "CMakeFiles/RayMarching.dir/RayMarching.cpp.obj.d"
+  "CMakeFiles/RayMarching.dir/glad.c.obj"
+  "CMakeFiles/RayMarching.dir/glad.c.obj.d"
+  "CMakeFiles/RayMarching.dir/stb_image/stb_image.cpp.obj"
+  "CMakeFiles/RayMarching.dir/stb_image/stb_image.cpp.obj.d"
+  "RayMarching.exe"
+  "RayMarching.exe.manifest"
+  "RayMarching.pdb"
+  "libRayMarching.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/RayMarching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
